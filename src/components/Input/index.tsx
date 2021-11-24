eef962361d0b27fa752a7e2e85ff9d83d013854b
savio591 <savio591@hotmail.com>
2021-11-24 14:44:58 -0300

    [Ignite/ReactJS] - Desafio 02 - Desafio Concluído! 🚀

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,13 +3,19 @@ import {
   useRef,
   useState,
   useCallback,
+  InputHTMLAttributes,
 } from 'react';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon?: any;
+}
+
+const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
   const inputRef = useRef(null);
 
   const [isFocused, setIsFocused] = useState(false);
